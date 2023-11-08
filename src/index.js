96ad6d1c6b7bff338084098304b12b19e4ee2ead
Bharath-Viswam <79804086+Bharath-Viswam@users.noreply.github.com>
2023-11-08 12:24:05 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+//test
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
